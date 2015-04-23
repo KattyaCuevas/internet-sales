23e653f567f7972257bd0cbe8baf1f59e6f0c341
Kattya Cuevas <kattya.cuevas@gmail.com>
2015-04-23 18:41:54 -0500

    add role

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.2.0'
 gem 'wicked_pdf'
 gem 'wkhtmltopdf-binary'
 gem 'devise'
+gem 'cancan'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423175649) do
+ActiveRecord::Schema.define(version: 20150423231545) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150423175649) do
     t.datetime "updated_at"
     t.string   "nombre"
     t.string   "apellido"
+    t.string   "rol"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
